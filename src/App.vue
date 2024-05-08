8f71fc3ac11e3de7e4c0631540e8353e379c88c7
loris.gschwind <loris.gschwind@divcom.ch>
2024-05-08 10:48:01 +0200

    ajout style dynamique

--- src/App.vue
+++ src/App.vue
@@ -64,30 +64,30 @@ const troupes = ref([
     <ul class="cartes">
       <li v-for="troupe in troupes" :key="troupe.id">
         <article>
-          <header style="background: linear-gradient(60deg,#3B3B3B 0%, #EE5487 100%);">
+          <header :style="'background: linear-gradient(60deg,#3B3B3B 0%, ' + troupe.couleur + ' 100%);'">
             <img :src="troupe.image"
                  :alt="troupe.nom">
           </header>
-          <div class="level" style="color: #EE5487;">
+          <div class="level" :style="'color:'+ troupe.couleur+ ' ;'">
            Niveau {{ troupe.niveau }}
           </div>
           <h2 class="name">{{troupe.nom}}</h2>
-          <button style="background-color: #EE5487;"> Former
+          <button :style="'background-color: ' + troupe.couleur +';'"> Former
             <img src="/img/piece-or.png" alt="Former"></button>
           <p class="description">{{troupe.description}}</p>
           <footer>
             <div class="training"
-                 style="background-color: #EE5487;">
+                 :style="'background-color: ' + troupe.couleur +';'">
               <div>{{ troupe.formation }}<sup>sec</sup></div>
               <div>Formation</div>
             </div>
             <div class="speed"
-                 style="background-color: #EE5487;">
+                 :style="'background-color: ' + troupe.couleur +';'">
               <div>{{ troupe.vitesse }}</div>
               <div>Vitesse</div>
             </div>
             <div class="cost"
-                 style="background-color: #EE5487;">
+                 :style="'background-color: ' + troupe.couleur +';'">
               <div>{{ troupe.cout }}</div>
               <div>Coût</div>
             </div>
